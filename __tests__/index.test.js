a2d3d3b989ec0a6619d949554c7623a5dafeb948
Arthur Buravljov <buravlev-arthur@yandex.ru>
2022-04-28 19:07:44 +0300

    added cap for tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
 /* eslint no-undef: "off" */
+import sum from '../src/temp.js';
 
 test('check sum', () => {
-  const s = 5 + 2;
-  expect(s).toBe(7);
+  const sum1 = sum(5, 2);
+  expect(sum1).toBe(7);
 });
