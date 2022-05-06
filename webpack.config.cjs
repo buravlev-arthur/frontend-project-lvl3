135b23eac038e3a3d1a73c40997e171cbfd5541d
Arthur Buravljov <buravlev-arthur@yandex.ru>
2022-05-06 20:53:44 +0300

    add main=index.js in package.json

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -9,7 +9,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, '/src/index.js'),
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'index.html'),
